8889fe73894b3bbef6ebc35253b3299dd2ec35c4
Yaniru <2223009802@qq.com>
2023-10-20 15:56:48 +0800

    feat(aerial): adjust window width

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },
     { import = "plugins.extras.neovide" },
+    { import = "plugins.extras.aerial" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ lua/plugins/extras/aerial.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    optional = true,
+    opts = {
+      layout = {
+        -- max width is the lesser of 40 cols or 30% of total
+        max_width = { 40, 0.3 },
+        -- min width is the greater of 20 cols or 20% of total
+        min_width = { 20, 0.2 },
+      },
+    },
+  },
+}
