cc9b8424bc3e52579c4bb0762159172007012038
Yaniru <2223009802@qq.com>
2023-10-25 14:15:37 +0800

    feat(plugins): add fold-preview.nvim

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -16,4 +16,27 @@ return {
       require("ufo").setup()
     end,
   },
+
+  -- fold preview
+  {
+    "anuvyklack/fold-preview.nvim",
+    event = "VeryLazy",
+    dependencies = "anuvyklack/keymap-amend.nvim",
+    config = function()
+      local fp = require("fold-preview")
+      local keymap = vim.keymap
+      keymap.amend = require("keymap-amend")
+
+      fp.setup({
+        border = "single",
+        default_keybindings = false,
+      })
+
+      keymap.amend("n", "h", function(original)
+        if not fp.toggle_preview() then
+          original()
+        end
+      end)
+    end,
+  },
 }
