b7ce7a19f16da4db75aa6c056f06584f38e78086
Yaniru <2223009802@qq.com>
2023-10-31 20:10:54 +0800

    fix(snippet): stop snippets when leave to normal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,17 @@
 local function augroup(name)
   return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
 end
+
+-- stop snippets when leave to normal mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = augroup("stop_snippets"),
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
