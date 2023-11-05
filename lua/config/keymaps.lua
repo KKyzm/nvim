8a9c0363482d0b38c92742362741c48de3a7e36b
Yaniru <2223009802@qq.com>
2023-11-05 23:31:22 +0800

    feat(keymaps): "yd" to copy diagnose message

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,22 @@ vim.keymap.set("n", "*", ":keepjumps normal! mi*`i<Cr>")
 
 -- Clear hlsearch with <esc>
 vim.keymap.set("n", "<esc>", "<Cmd>nohlsearch<CR>", { desc = "clear hlsearch" })
+
+-- Copy diagnose message of current line only into system clipboard
+vim.keymap.set("n", "yd", function()
+  local line_nr, col_nr = unpack(vim.api.nvim_win_get_cursor(0))
+  local diagnoses = vim.diagnostic.get(0)
+  if next(diagnoses) == nil then
+    vim.notify("No diagnose message avaliable", vim.log.levels.WARN)
+    return
+  end
+  for _, diagnose in ipairs(diagnoses) do
+    if diagnose.lnum + 1 == line_nr and diagnose.col == col_nr then
+      vim.fn.setreg("+", diagnose.message)
+      vim.notify('"' .. diagnose.message .. '"', vim.log.levels.INFO, { title = "Diagnose message copied" })
+      break
+    end
+  end
+end, {
+  desc = "Copy diagnose message",
+})
